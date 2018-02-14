5369541726bc9357466a052920011be2253cccff
Danny-Martins <Danny_Martins50@hotmail.com>
2018-02-13 22:38:44 -0500

    Tower, Enemy, GameController
    Tower : Added enemy targeting , basic stats
    GameController: Added temp basic scene management, object loading and
    error handling
    Enemy : Added very basic stats and goal detection

---
+++ Project Feature/Assets/Resources/Enemies/EnemyController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class EnemyController : MonoBehaviour {
+
+	[SerializeField] protected float health = 1;
+	/// move speed in units per frame
+	[SerializeField] protected float move_speed = 1;
+
+	virtual protected void OnTriggerEnter(Collider other_collider){
+		if(other_collider.tag != "Enemy Finishline")
+			return;
+
+		Destroy(this.gameObject);
+		//lose lives/gameover
+	}
+
+}
---
+++ Project Feature/Assets/Resources/Towers/Tower.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System;
+using UnityEngine;
+
+abstract public class Tower : MonoBehaviour {
+
+	[SerializeField] protected float damage = 1;
+	[SerializeField] protected float range = 1;
+	/// rate of fire per second
+	[SerializeField] protected float fire_rate = 1;
+
+	[SerializeField] protected List<EnemyController> targets_in_range = null;
+	[SerializeField] protected EnemyController current_target = null;
+
+	protected bool can_shoot = true;
+
+	virtual protected void Start(){
+		this.targets_in_range = new List<EnemyController> ();
+	}
+
+	virtual protected void OnTriggerEnter(Collider other_collider){
+		if(other_collider.tag != "Enemy")
+			return;
+
+		EnemyController enemy = other_collider.GetComponent<EnemyController> ();
+
+		this.targets_in_range.Add(enemy);
+
+		if(!this.current_target)
+			this.current_target = enemy;
+	}
+
+	virtual protected void OnTriggerExit(Collider other_collider){
+		if(other_collider.tag != "Enemy")
+			return;
+
+		EnemyController enemy = other_collider.GetComponent<EnemyController> ();
+
+		if(this.current_target == enemy){
+			this.current_target = null;
+			this.retarget();
+			return;
+		}
+
+		try{
+			this.targets_in_range.Remove(enemy);
+		}
+		catch (Exception exception){
+			throw new NotImplementedException();
+		}
+
+	}
+
+	virtual protected IEnumerator reload(){
+		yield return new WaitForSeconds(this.fire_rate);
+		this.can_shoot = true;
+	}
+
+	virtual protected void retarget(){
+		if(this.current_target || this.targets_in_range.Count == 0)
+			return;
+
+		foreach (EnemyController enemy in this.targets_in_range){
+			if(!enemy){
+				this.targets_in_range.Remove(enemy);
+				continue;
+			}
+				
+			this.current_target = enemy;
+			return;
+		}
+	}
+
+}
